ddb19bbd037fef2ee8f0dac8a757c47df791f715
Leo <39918089+Lihang99@users.noreply.github.com>
2020-10-21 09:50:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-adb forward tcp:6100 localabstract:miniperfserver > /dev/null 2>&1
+adb forward tcp:45455 localabstract:miniperfserver > /dev/null 2>&1
 adb push app/build/intermediates/dex/debug/mergeDexDebug/classes.dex /data/local/tmp/MiniPerfServer.dex > /dev/null 2>&1
 adb shell mkdir /data/local/tmp/dalvik-cache > /dev/null 2>&1
 adb shell ANDROID_DATA=/data/local/tmp app_process -Djava.class.path=/data/local/tmp/MiniPerfServer.dex /data/local/tmp com.github.sandin.miniperfserver.MiniPerfServer $@
