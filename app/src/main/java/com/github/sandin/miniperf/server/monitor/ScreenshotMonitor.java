061a537d3f14cc4eae090ba9261e65ee995607c6
WuJi <710977702@qq.com>
2020-10-28 16:01:11 +0800

    修改截图旋转bug

--- app/src/main/java/com/github/sandin/miniperf/server/monitor/ScreenshotMonitor.java
+++ app/src/main/java/com/github/sandin/miniperf/server/monitor/ScreenshotMonitor.java
@@ -21,15 +21,23 @@ import java.util.Map;
 /**
  * Screenshot Monitor
  */
-public class ScreenshotMonitor implements IMonitor<Screenshot> {
+public class
+ScreenshotMonitor implements IMonitor<Screenshot> {
     private static final String TAG = "ScreenshotMonitor";
+
+    int rotation =0;
 
     @Override
     public Screenshot collect(TargetApp targetApp, long timestamp, ProfileNtf.Builder data) throws Exception {
         Screenshot.Builder builder = Screenshot.newBuilder();
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         takeScreenshot(byteArrayOutputStream);
-        Screenshot screenshot = builder.setData(ByteString.copyFrom(byteArrayOutputStream.toByteArray())).build();
+        if(rotation==2){
+            rotation = 3;
+        }else if(rotation==3){
+            rotation = 2;
+        }
+        Screenshot screenshot = builder.setData(ByteString.copyFrom(byteArrayOutputStream.toByteArray())).setOrientationValue(rotation).build();
         if (data != null)
             data.setScreenshot(screenshot);
         return screenshot;
@@ -48,7 +56,7 @@ public class ScreenshotMonitor implements IMonitor<Screenshot> {
         Size size = displayInfo.getSize();
         int width = size.getWidth() / 3;
         int height = size.getHeight() / 3;
-        int rotation = displayInfo.getRotation();
+        rotation = displayInfo.getRotation();
         Log.i(TAG, "screen width: " + width + ", height: " + height + ", rotation:" + rotation);
 
         long start = System.nanoTime();
